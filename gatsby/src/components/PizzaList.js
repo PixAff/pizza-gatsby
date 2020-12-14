6f6d76e9f11a0a17d7dba1be4a9bd6cd1704226b
Stefan Sarow <stefan@pixelaffairs.com>
2020-12-14 12:35:36 +0100

    added topping tags to pizza page

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,26 +1,46 @@
 import React from "react";
 import { Link } from "gatsby";
 import Img from "gatsby-image";
+import styled from "styled-components";
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaSyles = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3 / auto;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const PizzaList = ({ pizzas }) => {
-  console.log(pizzas[0].name);
-
   // eslint-disable-next-line arrow-body-style
   const singlePizza = pizzas.map((pizza) => {
     return (
-      <div key={pizza.id}>
+      <PizzaSyles key={pizza.id}>
         <Link to={`/pizza/${pizza.slug.current}`}>
           <h2>
             <span className="mark">{pizza.name}</span>
           </h2>
-          <p>{pizza.toppings.map((topping) => topping.name).join(", ")}</p>
-          <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
         </Link>
-      </div>
+        <p>{pizza.toppings.map((topping) => topping.name).join(", ")}</p>
+        <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+      </PizzaSyles>
     );
   });
 
-  return <div>{singlePizza}</div>;
+  return <PizzaGridStyles>{singlePizza}</PizzaGridStyles>;
 };
 
 export default PizzaList;
---
+++ gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import styled from "styled-components";
+
+const ToppingStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
+
+function CountPizzasInToppings(pizzas) {
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      // check if this is an existing topping
+      const existingTopping = acc[topping.id];
+      if (existingTopping) {
+        // if it is, increment by 1
+        existingTopping.count += 1;
+      } else {
+        // otherwise create new entry and set to 1
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+  // sort them based on count
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
+}
+
+export default function ToppingsFilter() {
+  const { toppings, pizzas } = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+          vegetarian
+        }
+      }
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            name
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  const toppingsWithCounts = CountPizzasInToppings(pizzas.nodes);
+  console.clear();
+  console.log(toppingsWithCounts);
+
+  const toppingTags = toppingsWithCounts.map((topping) => {
+    return (
+      <Link key={topping.id} to={`/toppings/${topping.name}`}>
+        <span className="name">{topping.name}</span>
+        <span className="count">{topping.count}</span>
+      </Link>
+    );
+  });
+
+  return <ToppingStyles>{toppingTags}</ToppingStyles>;
+}
