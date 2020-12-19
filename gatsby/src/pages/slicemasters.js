68646e67fcdc554219e386c59c2bf48eab185038
Stefan Sarow <stefan@pixelaffairs.com>
2020-12-19 21:16:25 +0100

    started slicemasters

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,5 +1,81 @@
 import React from "react";
+import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
 
-export default function SliceMastersPage() {
-  return <div>SliceMastersPage</div>;
+const MasterGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const MasterSyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    position: relative;
+    z-index: 2;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+export default function SliceMastersPage({ data, pageContext }) {
+  const masters = data.persons.nodes;
+  console.log(masters);
+
+  return (
+    <MasterGridStyles>
+      {masters.map((master) => (
+        <MasterSyles key={master.id}>
+          <Link to={`/slicemaster/${master.slug.current}`}>
+            <h2>
+              <span className="mark">{master.name}</span>
+            </h2>
+          </Link>
+          <Img fluid={master.image.asset.fluid} alt={master.name} />
+          <p className="description">{master.description}</p>
+        </MasterSyles>
+      ))}
+    </MasterGridStyles>
+  );
 }
+
+export const query = graphql`
+  query SliceMasterQuery {
+    persons: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        slug {
+          current
+        }
+        id
+        description
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
